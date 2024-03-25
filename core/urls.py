f9e71f5052fe707e4da7039d1bb02725411ad9f8
Julio Kozarewicz <julioerk@outlook.com>
2024-03-25 19:32:23 -0300

    [ SERCH ENGINE ] (DESENVOLVIMENTO) Pesquisa de itens.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, search
 
 app_name = 'core'
 
-urlpatterns = [path('', home, name='home'), ]
+urlpatterns = [
+    path('', home, name='home'),
+    path('search', search, name='search'),
+]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def home(request):
     template_name='core/home.html'
     return render(request, template_name)
+
+def search(request):
+    template_name='core/search.html'
+    return render(request, template_name)
