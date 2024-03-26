b91d452c26cb0dac1a904bd2be2c4803f13f396d
Julio Kozarewicz <julioerk@outlook.com>
2024-03-26 12:58:04 -0300

    [ MENSAGENS ] ( finalizado ) Sistema de feedback push up no site.

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from django.contrib import messages
 
 def home(request):
     template_name='core/home.html'
@@ -34,10 +34,14 @@ class insert(CreateView):
     template_name = 'core/insert.html'
     context_object_name = 'insert'
 
+    message_ok ='Livro inserido no sistema.'
+    messade_erro = 'Não foi possível realizar a tarefa!'
+
     def form_valid(self, form):
         obj = form.save(commit=False)
         obj.user = self.request.user
         obj.save()
+        messages.add_message(self.request, messages.INFO, self.message_ok)
         return super().form_valid(form)
 
 @method_decorator(login_required, name='dispatch')
@@ -46,6 +50,13 @@ class delete(DeleteView):
     success_url = reverse_lazy('core:search')
     template_name = 'core/search.html'
 
+    message_ok ='Livro apagado do sistema.'
+    messade_erro = 'Não foi possível realizar a tarefa!'
+
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.INFO, self.message_ok)
+        return super().form_valid(form)
+
 @method_decorator(login_required, name='dispatch')
 class edit(UpdateView):
     model = booksDataBase
@@ -53,8 +64,12 @@ class edit(UpdateView):
     template_name = 'core/edit.html'
     context_object_name = 'edit'
 
+    message_ok ='Livro editado com sucesso.'
+    messade_erro = 'Não foi possível realizar a tarefa!'
+
     def form_valid(self, form):
         obj = form.save(commit=False)
         obj.user = self.request.user
         obj.save()
+        messages.add_message(self.request, messages.INFO, self.message_ok)
         return super().form_valid(form)
