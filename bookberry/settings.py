f1894073b20b550749ee14bf257125b406776146
Julio Kozarewicz <julioerk@outlook.com>
2024-03-26 11:03:57 -0300

    [ LOGOUT ] ( lógica ) Desabilitar confimação de sair.

--- bookberry/settings.py
+++ bookberry/settings.py
@@ -165,4 +165,6 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+ACCOUNT_LOGOUT_ON_GET = True
 # ----------------------------------------------------------------------------
